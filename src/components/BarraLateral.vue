244d8480b6525960aa550c547901d023ceaf8f51
Citer <citer5@hotmail.com>
2024-10-13 21:03:50 -0600

    commit

---
+++ src/components/BarraLateral.vue
@@ -0,0 +1,21 @@
+<template>
+	<h1 id="titleATM" class="bold">ATM</h1>
+	<div class="BLTitle">
+		<h3 class="titleBL">Bienvenido</h3>
+		<p>Stephen Curry</p>
+	</div>
+	<div class="BLTitle">
+		<h3 class="titleBL">No. Cuenta</h3>
+		<p>Cuenta #1</p>
+	</div>
+	<div class="BLTitle">
+		<h3 class="titleBL">No. Tarjeta</h3>
+		<p>1111 2222 3333 4444</p>
+	</div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
---
+++ src/components/LayoutDefault.vue
@@ -0,0 +1,20 @@
+<template>
+	<div class="container">
+		<div id="barralateral">
+			<BarraLateral />
+		</div>
+		<main class="content"></main>
+	</div>
+</template>
+
+<script>
+import BarraLateral from "./BarraLateral.vue";
+
+export default {
+	components: {
+		BarraLateral,
+	},
+};
+</script>
+
+<style></style>
